42a3d3893174c62460ac2237f2215834e78ffa82
David Janssen <janssen.dhj@gmail.com>
2019-10-03 10:30:15 -0500

    Starting type for special symbols

---
+++ src/KMonad/Core/SpecialSymbol.hs
@@ -0,0 +1,49 @@
+module KMonad.Core.SpecialSymbol
+
+where
+
+import Control.Lens
+import Data.Text (Text)
+
+import KMonad.Core.Keyboard
+import KMonad.Core.KeyCode
+
+data SpecialSymbol = SpecialSymbol
+  { _name       :: Text
+  , _chr        :: Char
+  , _composeSeq :: Maybe [KeySequence]
+  }
+makeClassy ''SpecialSymbol
+
+
+specialSymbols :: [SpecialSymbol]
+specialSymbols = let ss (a, b, c) = SpecialSymbol a b c in map ss
+  [ -- Currency
+    ("Euro sign"    , '€', Just [KeyEqual, KeyC])
+  , ("Pount sign"   , '₤', Just [KeyEqual, KeyL])
+
+    -- Fractions
+  , ("One-half"     , '½', Just [Key1, Key2])
+  , ("Two-thirds"   , '⅔', Just [Key2, Key3])
+
+
+
+  ]
+
+--------------------------------------------------------------------------------
+-- $unicode
+
+-- newtype Unicode = Unicode Int
+--   deriving (Eq, Show, Bounded, Enum, Ord)
+
+-- fromChar :: Char -> Unicode
+-- fromChar = Unicode . fromEnum
+
+-- showSeq :: Unicode -> T.Text
+-- showSeq (Unicode c) = T.pack (showIntAtBase 16 intToDigit c $ "")
+
+-- test :: Char
+-- test = 'Ñ'
+
+-- test2 :: Char
+-- test2 = 'ñ'
