9db7478c8ad19946078d3832fe7c8cf1a47c03bc
David Janssen <janssen.dhj@gmail.com>
2019-10-09 11:47:49 -0500

    Added CPP-based dispatch code

--- src/KMonad/Api/Encode.hs
+++ src/KMonad/Api/Encode.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-|
 Module      : KMonad.Api.Encode
 Description : Translate tokens to their App-equivalents
@@ -6,6 +7,12 @@ License     : MIT
 Maintainer  : janssen.dhj@gmail.com
 Stability   : experimental
 Portability : portable
+
+Essentially, everything in KMonad should simply compile normally, regardless of
+OS. We use cabal-file conditionals to include/exclude OS-specific code on
+different platforms, but we need 1 spot where we determine how to dispatch
+encoding based on the operating system. That is *this* module.
+
 
 -}
 module KMonad.Api.Encode
@@ -18,13 +25,26 @@ import Control.Lens
 import Data.Maybe
 
 import KMonad.Core.Parser
-import KMonad.Api.KeyIO
-import KMonad.Api.KeyIO.Linux.DeviceSource
-import KMonad.Api.KeyIO.Linux.UinputSink
 
+import KMonad.Api.KeyIO
 import qualified Data.Text as T
 
---------------------------------------------------------------------------------
+
+-- Linux only imports
+#ifdef linux_HOST_OS
+import KMonad.Api.KeyIO.Linux.DeviceSource
+import KMonad.Api.KeyIO.Linux.UinputSink
+#endif
+
+#ifdef mingw32_HOST_OS
+import System.Win32
+#endif
+
+-- Windows only import
+
+-- Linux dispatch
+
+#ifdef linux_HOST_OS
 
 -- | Translate an 'InputToken' to a 'KeySource' object
 pickInputIO :: InputToken -> KeySource
@@ -37,3 +57,15 @@ pickOutputIO (UinputDevice nm pi) = let def = defUinputCfg in
     { _keyboardName = fromMaybe (def^.keyboardName) (T.unpack <$> nm)
     , _postInit     = T.unpack <$> pi
     }
+
+#endif
+
+#ifdef mingw32_HOST_OS
+
+pickInputIO :: InputToken -> KeySource
+pickInputIO _ = error "not implemented yet"
+
+pickOutputIO :: OutputToken -> KeySink
+pickOutputIO _ = error "not implemented yet"
+
+#endif
