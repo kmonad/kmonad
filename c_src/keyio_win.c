334b59c4ea433738f6017ad82a5a89b25add1c18
David Janssen <janssen.dhj@gmail.com>
2019-10-09 18:06:58 -0700

    notes

--- c_src/keyio_win.c
+++ c_src/keyio_win.c
@@ -1,3 +1,11 @@
 #include <windows.h>
 #include <stdio.h>
 
+
+hook = SetWindowsHookEx(WH_KEYBOARD_LL, callback, hwnd, 0);
+
+LRESULT callback(int nCode, WPARAM wParam, LPARAM lParam) {
+
+};
+
+//HHOOK kbHook = SetWindowsHookEx(WH_KEYBOARD_LL, &callback, GetModuleHandle(NULL), NULL);
