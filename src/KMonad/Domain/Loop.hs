33a63a5dca6a8e02253f9a0cf7dc4b8d90179bb9
David Janssen <janssen.dhj@gmail.com>
2019-10-15 10:14:43 -0500

    Hooked up MonadLogger

--- src/KMonad/Domain/Loop.hs
+++ src/KMonad/Domain/Loop.hs
@@ -14,14 +14,15 @@ module KMonad.Domain.Loop
   )
 where
 
+import Control.Monad.Logger
 import Control.Lens
 import KMonad.Core
 import KMonad.Domain.Effect
 
 -- | Loop over events, pass 'Press' and 'Release' events to the handler while
 -- ignoring 'Repeat' events. Break the loop on a 'Quit' event.
-loop :: (MonadNext m, MonadTrace m, MonadHandler m) => m ()
-loop = trace "Starting event loop" >> loop'
+loop :: (MonadNext m, MonadTrace m, MonadHandler m, MonadLogger m) => m ()
+loop = $(logInfo) "Starting event loop" >> loop'
   where loop' = nextEvent >>= \case
           Quit          -> trace "Exiting"
           InputEvent e  -> case e^._type of
