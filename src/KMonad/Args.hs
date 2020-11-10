a6b5b28ad52c90d34d3584fd6ad130b3ae1c0168
slotThe <soliditsallgood@mailbox.org>
2020-11-10 12:46:15 +0100

    Insert command line options into cfg options
    Command line options are preferred (if necessary overwriting the ones given in
    the configuration file).

--- src/KMonad/Args.hs
+++ src/KMonad/Args.hs
@@ -36,15 +36,15 @@ runCmd :: Cmd -> IO ()
 runCmd c = do
   o <- logOptionsHandle stdout False <&> setLogMinLevel (c^.logLvl)
   withLogFunc o $ \f -> runRIO f $ do
-    cfg <- loadConfig $ c^.cfgFile
+    cfg <- loadConfig c
     unless (c^.dryRun) $ startApp cfg
 
 -- | Parse a configuration file into a 'AppCfg' record
-loadConfig :: HasLogFunc e => FilePath -> RIO e AppCfg
-loadConfig pth = do
+loadConfig :: HasLogFunc e => Cmd -> RIO e AppCfg
+loadConfig cmd = do
 
-  tks <- loadTokens pth   -- This can throw a PErrors
-  cgt <- joinConfigIO tks -- This can throw a JoinError
+  tks <- loadTokens (cmd^.cfgFile)      -- This can throw a PErrors
+  cgt <- joinConfigIO (joinCLI cmd tks) -- This can throw a JoinError
 
   -- Try loading the sink and src
   lf  <- view logFuncL
@@ -60,3 +60,35 @@ loadConfig pth = do
     , _fallThrough  = _flt   cgt
     , _allowCmd     = _allow cgt
     }
+
+-- | Join the options given from the command line with the one read from the
+-- configuration file.
+-- This does not yet throw any kind of exception, as we are simply inserting the
+-- given options into every 'KDefCfg' block that we see.
+joinCLI :: Cmd -> [KExpr] -> [KExpr]
+joinCLI cmd = traverse._KDefCfg %~ insertCliOption cliList
+ where
+  -- | All options and flags that were given on the command line.
+  cliList :: DefSettings
+  cliList = catMaybes $
+       map flagToMaybe [cmd^.cmdAllow, cmd^.fallThrgh]
+    <> [cmd^.iToken, cmd^.oToken, cmd^.cmpSeq, cmd^.initStr]
+
+  -- | Convert command line flags to a 'Maybe' type, where the non-presence, as
+  -- well as the default value of a flag will be interpreted as @Nothing@
+  flagToMaybe :: DefSetting -> Maybe DefSetting
+  flagToMaybe = \case
+    SAllowCmd    b -> if b then Just (SAllowCmd    b) else Nothing
+    SFallThrough b -> if b then Just (SFallThrough b) else Nothing
+    _              -> Nothing
+
+  -- | Insert all command line options, potentially overwriting already existing
+  -- options that were given in the configuration file. This is a paramorphism
+  insertCliOption :: DefSettings -> DefSettings -> DefSettings
+  insertCliOption cliSettings cfgSettings =
+    foldr (\s cfgs ->
+             if   s `elem` cfgs
+             then foldr (\x xs -> (if s == x then s else x) : xs) [] cfgs
+             else s : cfgs)
+          cfgSettings
+          cliSettings
