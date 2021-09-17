f49a41c6ed6b871874ba1beee953fc8e0377a932
David Janssen <janssen.dhj@gmail.com>
2021-09-17 07:57:18 +0200

    Fix a build-issue on nix
    We started using `git` in some template-haskell code, which causes us to
    depend on git to build KMonad. This escaped everyone's notice on stack,
    because everyone has `git` installed globally, but it broke the `nix`
    build.
    
    Fixed this issue, and also updated nix infrastructure to use `niv` to
    manage dependencies.
    
    Squashed commit of the following:
    
    commit 7ee88301d96f93bed2151a97642a759e10ceb23e
    Author: David Janssen <janssen.dhj@gmail.com>
    Date:   Fri Sep 17 07:56:16 2021 +0200
    
        Moved changes to ./nix subfolder
    
    commit a5ed96628aabdd3443b07262ac4a6532d5de596a
    Author: David Janssen <janssen.dhj@gmail.com>
    Date:   Fri Sep 17 07:27:05 2021 +0200
    
        First new nix infrastructure

--- nix/default.nix
+++ nix/default.nix
@@ -1,5 +1,29 @@
+{ sources ? import ./sources.nix, pkgs ? (import sources.nixpkgs { }) }:
+
 let
-  # config = import ./config.nix;
-  pkgs   = import (import ./pinned-nixpkgs.nix) {};
-  kmonad = import ./kmonad.nix;
-in with pkgs.haskellPackages; callPackage kmonad {}
+  t = pkgs.lib.trivial;
+  hl = pkgs.haskell.lib;
+
+  deps = [
+    pkgs.git # Necessary to compile KMonad, but not to run it.
+  ];
+
+  pkg = pkgs.haskellPackages.developPackage {
+
+    root = ./..;
+
+    modifier = (t.flip t.pipe) [
+      (drv: hl.addBuildDepends drv deps) # Insert our buildDepends
+      hl.justStaticExecutables # Only build the executable
+
+      # TODO: investigate these here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/lib.nix
+      # hl.dontHaddock
+      # hl.enableStaticLibraries
+      # hl.disableLibraryProfiling
+      # hl.disableExecutableProfiling
+      #
+      # Maybe I make multiple targets, 1 for the executable, 1 for the hackage docs?
+    ];
+  };
+
+in { inherit pkg; }
--- nix/pinned-nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/5b36111f3c7a0468d099516832418e4e350fde6c.tar.gz";
-  sha256 = "1a8v85xaqp46ccfihpi20vwrb1bvqhpsfnasq8klg92qqwq9wiiy";
-}
