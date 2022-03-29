c85e4ff9241b2e983cbab0738feea7e7a134a0fb
MuhammedZakir <8190126+MuhammedZakir@users.noreply.github.com>
2022-03-30 03:14:51 +0530

    Mac: list-keyboards: Print all available identifiers

--- c_src/mac/list-keyboards.c
+++ c_src/mac/list-keyboards.c
@@ -1,5 +1,8 @@
 #include <IOKit/hid/IOHIDLib.h>
 #include <IOKit/hidsystem/IOHIDShared.h>
+#include <stdlib.h> // free
+
+#include "device_properties.h"
 
 int main() {
     CFMutableDictionaryRef matching_dictionary = IOServiceMatching(kIOHIDDeviceKey);
@@ -7,16 +10,20 @@ int main() {
         fprintf(stderr,"IOServiceMatching failed");
         return 1;
     }
+
     UInt32 value;
     CFNumberRef cfValue;
+
     value = kHIDPage_GenericDesktop;
     cfValue = CFNumberCreate( kCFAllocatorDefault, kCFNumberSInt32Type, &value );
     CFDictionarySetValue(matching_dictionary, CFSTR(kIOHIDDeviceUsagePageKey), cfValue);
     CFRelease(cfValue);
+
     value = kHIDUsage_GD_Keyboard;
     cfValue = CFNumberCreate( kCFAllocatorDefault, kCFNumberSInt32Type, &value );
     CFDictionarySetValue(matching_dictionary,CFSTR(kIOHIDDeviceUsageKey),cfValue);
     CFRelease(cfValue);
+
     io_iterator_t iter = IO_OBJECT_NULL;
     kern_return_t r = IOServiceGetMatchingServices(kIOMasterPortDefault,
                                                    matching_dictionary,
@@ -25,12 +32,22 @@ int main() {
         fprintf(stderr,"IOServiceGetMatchingServices failed");
         return r;
     }
-    for(mach_port_t curr = IOIteratorNext(iter); curr; curr = IOIteratorNext(iter)) {
-        CFTypeRef str = IORegistryEntryCreateCFProperty(curr,
-                                                        CFSTR("Product"),
-                                                        kCFAllocatorDefault,
-                                                        kIOHIDOptionsTypeNone);
-        CFShow(str);
+
+    struct device_properties *properties = malloc(sizeof *properties);
+    if (properties == NULL)
+    {
+        fprintf(stderr, "malloc error: device properties");
+        return 1;
     }
+
+    printf("Note: only prints non-empty fields.\n\n");
+    for(mach_port_t curr = IOIteratorNext(iter); curr; curr = IOIteratorNext(iter)) {
+        get_device_properties(curr, properties);
+        printf("=========================\n");
+        print_device_properties(properties);
+        printf("=========================\n");
+    }
+
+    free(properties);
     return 0;
 }
