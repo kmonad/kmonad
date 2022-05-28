2333a7fb7625ef7adcd27fc39a584a52222eb908
David Janssen <janssen.dhj@gmail.com>
2022-05-28 08:03:19 +0200

    Revert "Replace existing Nix code with a Nix flake (#549)"
    This reverts commit c5c4dfd617805f1a738f3346aa5d6ac4e9b56a4e.

---
+++ nix/default.nix
@@ -0,0 +1,28 @@
+{ sources ? import ./sources.nix, pkgs ? (import sources.nixpkgs { }) }:
+
+let
+  t = pkgs.lib.trivial;
+  hl = pkgs.haskell.lib;
+
+  deps = [
+    pkgs.git # Necessary to compile KMonad, but not to run it.
+  ];
+in
+pkgs.haskellPackages.developPackage {
+
+  root = ./..;
+  name = "kmonad";
+
+  modifier = (t.flip t.pipe) [
+    (drv: hl.addBuildDepends drv deps) # Insert our buildDepends
+    hl.justStaticExecutables # Only build the executable
+
+    # TODO: investigate these here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/lib.nix
+    # hl.dontHaddock
+    # hl.enableStaticLibraries
+    # hl.disableLibraryProfiling
+    # hl.disableExecutableProfiling
+    #
+    # Maybe I make multiple targets, 1 for the executable, 1 for the hackage docs?
+  ];
+}
