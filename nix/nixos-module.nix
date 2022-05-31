d7ef8d85cb6f620549e37022f02049b438b53c4e
David Janssen <janssen.dhj@gmail.com>
2022-05-31 08:27:13 +0200

    Replace existing Nix code with a Nix flake (#551)
    * Replace existing Nix code with a Nix flake
    
    Using the New Nix Flake
    =======================
    
    For Development
    ---------------
    
    If you use [direnv](https://direnv.net/) then the included top-level
    `.envrc` will load the development environment into your shell/editor
    for you.
    
    Otherwise, change into the `nix` directory and run `nix develop` and
    you will be dropped into a shell with all of the Haskell tools you
    expect.
    
    Using the KMonad NixOS Module
    -----------------------------
    
    Create your own `flake.nix` that looks something like this:
    
    ```nix
    {
      inputs = {
        nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
        kmonad.url = "github:kmonad/kmonad?dir=nix";
      };
    
      outputs = { self, nixpkgs, kmonad }: {
        nixosConfigurations.my_host_name = nixpkgs.lib.nixosSystem {
          system = "x86_64-linux";
          modules = [
            kmonad.nixosModules.default
            ./path/to/configuration.nix
          ];
        };
      };
    }
    ```
    
    More information can be found here:
    
      <https://nixos.wiki/wiki/Flakes>
    
    Building KMonad with Various Versions of GHC
    --------------------------------------------
    
        $ cd nix
        $ nix build .#kmonad-ghc8107
        $ nix build .#kmonad-ghc902
        $ nix build .#kmonad-ghc922
    
    Running the NixOS Virtual Machine Tests
    ---------------------------------------
    
        $ cd nix
        $ nix flake check
    
    * nix: Create devShells that mirror the package names
    
    This allows you to drop into a shell where the package, compiler, and
    support tools all match.  However, this (currently) only works with
    GHC 9.0.2 since various tooling for the other compilers is currently
    broken.
    
    Co-authored-by: Peter Jones <pjones@devalot.com>

--- nix/nixos-module.nix
+++ nix/nixos-module.nix
@@ -1,11 +1,157 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  cfg = config.services.kmonad;
+
+  # Per-keyboard options:
+  keyboard = { name, ... }: {
+    options = {
+      name = lib.mkOption {
+        type = lib.types.str;
+        example = "laptop-internal";
+        description = "Keyboard name.";
+      };
+
+      device = lib.mkOption {
+        type = lib.types.path;
+        example = "/dev/input/by-id/some-dev";
+        description = "Path to the keyboard's device file.";
+      };
+
+      extraGroups = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+        example = [ "openrazer" ];
+        description = ''
+          Extra permission groups to attach to the KMonad instance for
+          this keyboard.
+
+          Since KMonad runs as an unprivileged user, it may sometimes
+          need extra permissions in order to read the keyboard device
+          file.  If your keyboard's device file isn't in the input
+          group you'll need to list its group in this option.
+        '';
+      };
+
+      compose = {
+        key = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = "ralt";
+          description = "The (optional) compose key to use.";
+        };
+
+        delay = lib.mkOption {
+          type = lib.types.int;
+          default = 5;
+          description = "The delay (in milliseconds) between compose key sequences.";
+        };
+      };
+
+      fallthrough = lib.mkEnableOption "Reemit unhandled key events.";
+
+      allowCommands = lib.mkEnableOption "Allow keys to run shell commands.";
+
+      config = lib.mkOption {
+        type = lib.types.lines;
+        description = ''
+          Keyboard configuration excluding the defcfg block.
+        '';
+      };
+    };
+
+    config = {
+      name = lib.mkDefault name;
+    };
+  };
+
+  # Create a complete KMonad configuration file:
+  mkCfg = keyboard:
+    let defcfg = ''
+      (defcfg
+        input  (device-file "${keyboard.device}")
+        output (uinput-sink "kmonad-${keyboard.name}")
+    '' +
+    lib.optionalString (keyboard.compose.key != null) ''
+      cmp-seq ${keyboard.compose.key}
+      cmp-seq-delay ${toString keyboard.compose.delay}
+    '' + ''
+        fallthrough ${lib.boolToString keyboard.fallthrough}
+        allow-cmd ${lib.boolToString keyboard.allowCommands}
+      )
+    '';
+    in
+    pkgs.writeTextFile {
+      name = "kmonad-${keyboard.name}.cfg";
+      text = defcfg + "\n" + keyboard.config;
+      checkPhase = "${cfg.package}/bin/kmonad -d $out";
+    };
+
+  # Build a systemd path config that starts the service below when a
+  # keyboard device appears:
+  mkPath = keyboard: rec {
+    name = "kmonad-${keyboard.name}";
+    value = {
+      description = "KMonad trigger for ${keyboard.device}";
+      wantedBy = [ "default.target" ];
+      pathConfig.Unit = "${name}.service";
+      pathConfig.PathExists = keyboard.device;
+    };
+  };
+
+  # Build a systemd service that starts KMonad:
+  mkService = keyboard: {
+    name = "kmonad-${keyboard.name}";
+    value = {
+      description = "KMonad for ${keyboard.device}";
+      script = "${cfg.package}/bin/kmonad ${mkCfg keyboard}";
+      serviceConfig.Restart = "no";
+      serviceConfig.User = "kmonad";
+      serviceConfig.SupplementaryGroups = [ "input" "uinput" ] ++ keyboard.extraGroups;
+      serviceConfig.Nice = -20;
+    };
+  };
+in
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      kmonad = final.haskellPackages.callPackage ./default.nix { };
-    })
-  ];
+  options.services.kmonad = {
+    enable = lib.mkEnableOption "KMonad: An advanced keyboard manager.";
 
-  imports = [ ./module-base.nix ];
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.kmonad;
+      example = "pkgs.haskellPacakges.kmonad";
+      description = "The KMonad package to use.";
+    };
+
+    keyboards = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule keyboard);
+      default = { };
+      description = "Keyboard configuration.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+
+    users.groups.uinput = { };
+    users.groups.kmonad = { };
+
+    users.users.kmonad = {
+      description = "KMonad system user.";
+      group = "kmonad";
+      isSystemUser = true;
+    };
+
+    services.udev.extraRules = ''
+      # KMonad user access to /dev/uinput
+      KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+    '';
+
+    systemd.paths =
+      builtins.listToAttrs
+        (map mkPath (builtins.attrValues cfg.keyboards));
+
+    systemd.services =
+      builtins.listToAttrs
+        (map mkService (builtins.attrValues cfg.keyboards));
+  };
 }
