15db602313fce4909d24a6e1e2cad56d88d23a58
David Janssen <janssen.dhj@gmail.com>
2022-05-31 08:36:52 +0200

    Starting to reorganize modules

---
+++ src/KMonad/Prelude/Definitions.hs
@@ -0,0 +1,5 @@
+-- |
+
+module KMonad.Prelude.Definitions
+  ( )
+where
---
+++ src/KMonad/Prelude/Imports.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -Wno-dodgy-imports #-}
+
+module KMonad.Prelude.Imports
+  ( module X )
+where
+
+import Control.Lens       as X
+import Control.Monad.Cont as X
+import Data.Acquire       as X
+import GHC.Conc           as X (orElse)
+import RIO.Text           as X (unlines, lines, unpack, pack)
+
+import RIO as X hiding
+  (-- Not the lens stuff, I want more support for lenses from "Control.Lens"
+    view, ASetter, ASetter', Lens, Getting, Lens'
+  , SimpleGetter, lens, over, set, sets, to, (^.)
+
+    -- The following line is required for newer stack releases.
+    -- This is also the reason for the OPTIONS_GHC pragma
+  , (^..), (^?), preview, (%~), (.~)
+
+    -- Some stuff I'd rather default to Text
+  , unlines, lines
+
+    -- Will import these when I need it
+  , some, many
+  )
