1b657a591d7c755c6f83b7d36b4ffd8f5ebc8c31
Shi Rao <vosaica@outlook.com>
2023-09-30 02:52:30 -0400

    Updated code for breaking changes

--- c_src/mac/dext.cpp
+++ c_src/mac/dext.cpp
@@ -20,10 +20,6 @@ int init_sink() {
     pqrs::dispatcher::extra::initialize_shared_dispatcher();
     client = new pqrs::karabiner::driverkit::virtual_hid_device_service::client();
     auto copy = client;
-    client->async_driver_loaded();
-    client->async_driver_version_matched();
-    client->async_virtual_hid_keyboard_ready();
-    client->async_virtual_hid_pointing_ready();
     /**/
     client->connected.connect([copy] {
                                   std::cout << "connected" << std::endl;
@@ -38,19 +34,20 @@ int init_sink() {
     client->error_occurred.connect([](auto&& error_code) {
                                        std::cout << "error_occurred " << error_code << std::endl;
                                    });
-    client->driver_loaded_response.connect([](auto&& driver_loaded) {
+    client->driver_connected.connect([](auto&& driver_connected) {
                                                static std::optional<bool> previous_value;
 
-                                               if (previous_value != driver_loaded) {
-                                                   std::cout << "driver_loaded " << driver_loaded << std::endl;
-                                                   previous_value = driver_loaded;
+                                               if (previous_value != driver_connected) {
+                                                   std::cout << "driver_connected " << driver_connected << std::endl;
+                                                   previous_value = driver_connected;
                                                }
                                            });
-    client->driver_version_matched_response.connect([](auto&& driver_version_matched) {
+    client->driver_version_mismatched.connect([](auto&& driver_version_mismatched) {
                                                         static std::optional<bool> previous_value;
-                                                        if (previous_value != driver_version_matched) {
-                                                            std::cout << "driver_version_matched " << driver_version_matched << std::endl;
-                                                            previous_value = driver_version_matched;
+
+                                                        if (previous_value != driver_version_mismatched) {
+                                                            std::cout << "driver_version_mismatched " << driver_version_mismatched << std::endl;
+                                                            previous_value = driver_version_mismatched;
                                                         }
                                                     });
     /**/
