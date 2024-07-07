397ed3cc3ff2658b8e945cb331fbbfd234b277a1
Joschua Kesper <jokesper@proton.me>
2024-07-07 22:24:30 +0200

    Added `keymap/tutorial.kbd` to button doc test

--- test/KMonad/ButtonDocSpec.hs
+++ test/KMonad/ButtonDocSpec.hs
@@ -3,7 +3,8 @@ module KMonad.ButtonDocSpec (spec) where
 import KMonad.Args.Parser (keywordButtons)
 import KMonad.Prelude
 
-import RIO.Text (isInfixOf, isPrefixOf)
+import Data.Char (isSpace)
+import qualified RIO.Text as T
 
 import Test.Hspec
 
@@ -11,19 +12,22 @@ spec :: Spec
 spec = do
   (describe "button-docs" . traverse_ checkForDocsIn)
     [ "doc/quick-reference.md"
+    , "keymap/tutorial.kbd"
     ]
  where
   checkForDocsIn file = do
     cnt <- runIO $ readFileUtf8 file
     describe file . sequence_ $ traverse checkForDoc keywordButtons (lines cnt)
   checkForDoc btn cnt =
-    it ("Doc for " <> unpack (fst btn) <> " exists") $
-      unless (buttonDocExist btn cnt) (expectationFailure "button does not exist in documentation")
+    it ("Doc mentions " <> unpack (fst btn)) $
+      unless (buttonDocExist btn cnt) (expectationFailure "button is not mentioned in documentation")
   buttonDocExist = any . checkerFromButton . fst
   -- ignore deprecated names
   checkerFromButton "momentary-layer" = const True
   checkerFromButton "permanent-layer" = const True
   checkerFromButton btn =
-    isPrefixOf ("- `(" <> btn <> " ")
-      <||> isInfixOf ("`" <> btn <> "`")
+    T.isPrefixOf ("- `(" <> btn <> " ") . T.dropWhile isSpace
+      <||> T.isInfixOf ("`" <> btn <> "`")
+      <||> T.isInfixOf ("`" <> btn <> "'")
   (<||>) = liftA2 (||)
+  infixr 2 <||>
