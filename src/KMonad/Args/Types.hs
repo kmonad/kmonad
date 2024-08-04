3ca703c032f80c643fcf4c291a72038251bc68e4
Joschua Kesper <jokesper@proton.me>
2024-08-04 22:06:38 +0200

    Fix broken compose sequences v2
    This removes the duplicate sequence for `cent` and the non parseable sequences for nobreakspace and U2008
    
    (except for command line parameters (what the heck?!))

--- src/KMonad/Args/Types.hs
+++ src/KMonad/Args/Types.hs
@@ -89,7 +89,7 @@ data DefButton
   | KBeforeAfterNext DefButton DefButton   -- ^ Surround a future button in a before and after tap
   | KTrans                                 -- ^ Transparent button that does nothing
   | KBlock                                 -- ^ Button that catches event
-  deriving (Show, Typeable, Data)
+  deriving (Show, Eq, Typeable, Data)
 
 instance Plated DefButton
 
@@ -126,7 +126,7 @@ data DefSrc = DefSrc
   { _srcName  :: Maybe Text -- ^ A unique name used to refer to this layer.
   , _keycodes :: [Keycode]  -- ^ Layer settings containing also the buttons.
   }
-  deriving Show
+  deriving (Show, Eq)
 makeClassy ''DefSrc
 
 -- | A mapping from names to button tokens
@@ -138,7 +138,7 @@ data DefLayer = DefLayer
   , _associatedSrcName :: Maybe Text  -- ^ The source used by the layer
   , _buttons           :: [DefButton] -- ^ A list of button tokens
   }
-  deriving Show
+  deriving (Show, Eq)
 
 
 --------------------------------------------------------------------------------
@@ -194,7 +194,7 @@ data KExpr
   | KDefSrc   DefSrc
   | KDefLayer DefLayer
   | KDefAlias DefAlias
-  deriving Show
+  deriving (Show, Eq)
 makeClassyPrisms ''KExpr
 
 
