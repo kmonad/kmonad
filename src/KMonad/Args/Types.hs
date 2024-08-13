kmonad/kmonad

commit 3ca703c032f80c643fcf4c291a72038251bc68e4

Fix broken compose sequences v2
This removes the duplicate sequence for `cent` and the non parseable sequences for nobreakspace and U2008

(except for command line parameters (what the heck?!))

src/KMonad/Args/Types.hs
@@ -89,7 +89,7 @@ data DefButton
   | KBeforeAfterNext DefButton DefButton   -- ^ Surround a future button in a before and after tap
   | KTrans                                 -- ^ Transparent button that does nothing
   | KBlock                                 -- ^ Button that catches event
-  deriving (Show, Typeable, Data)
+  deriving (Show, Eq, Typeable, Data)
 
 instance Plated DefButton
 
@@ -126,7 +126,7 @@ data DefSrc = DefSrc
   { _srcName  :: Maybe Text -- ^ A unique name used to refer to this layer.
   , _keycodes :: [Keycode]  -- ^ Layer settings containing also the buttons.
   }
-  deriving Show
+  deriving (Show, Eq)
 makeClassy ''DefSrc
 
 -- | A mapping from names to button tokens
@@ -138,7 +138,7 @@ data DefLayer = DefLayer
   , _associatedSrcName :: Maybe Text  -- ^ The source used by the layer
   , _buttons           :: [DefButton] -- ^ A list of button tokens
   }
-  deriving Show
+  deriving (Show, Eq)
 
 
 --------------------------------------------------------------------------------
@@ -194,7 +194,7 @@ data KExpr
   | KDefSrc   DefSrc
   | KDefLayer DefLayer
   | KDefAlias DefAlias
-  deriving Show
+  deriving (Show, Eq)
 makeClassyPrisms ''KExpr
 
 

src/KMonad/Keyboard/ComposeSeq.hs
@@ -46,8 +46,6 @@ ssComposed = composeSeqs & (each . _1) %~ sanitize
     , ( "\" \""   , '¨'     , "diaeresis" )
     , ("spc <"    , 'ˇ'     , "caron")
     , (", spc"    , '¸'     , "cedilla")
-    , ("spc spc"  , ' '     , "nobreakspace")
-    , ("spc ."    , ' '     , "U2008")
     , ("o c"      , '©'     , "copyright")
     , ("o r"      , '®'     , "registered")
     , (". >"      , '›'     , "U203a")
@@ -736,7 +734,6 @@ ssComposed = composeSeqs & (each . _1) %~ sanitize
     , ("_ '"      , '⍘'     , "U2358")
     , ("0 ~"      , '⍬'     , "U236c")
     , ("| ~"      , '⍭'     , "U236d")
-    , ("c /"      , '¢'     , "cent" )
     , ("< _"      , '≤'     , "U2264")
     , ("> _"      , '≥'     , "U2265")
 
@@ -744,4 +741,6 @@ ssComposed = composeSeqs & (each . _1) %~ sanitize
     --, ("` spc"    , '`'     , "grave") -- recursive and incorrect. It's <dead_grave> <space> and <dead_grave> is not mapped in en_US
     --, ("^ spc", '^', "asciicircum") -- This overlaps with the normal 'shifted-6' macro for
     -- , ("' j", 'j́', "jacute")
+    --, ("spc spc"  , ' '     , "nobreakspace") -- cannot be parsed since it is matched by `lexeme` of previous token
+    --, ("spc ."    , ' '     , "U2008") -- see above
     ]

test/KMonad/ComposeSeqSpec.hs
@@ -9,12 +9,20 @@ import KMonad.Prelude
 
 import Test.Hspec
 
+import qualified RIO.Text as T
+
 spec :: Spec
 spec = describe "compose-sequences" $ traverse_ checkComposeSeq ssComposed
  where
-  checkComposeSeq (_, c, name) =
-    it ("Compose sequence for " <> unpack name <> " is valid") $
-      runParser buttonP "" (pack [c]) `shouldSatisfy` parsesAsValidComposeSeq
+  checkComposeSeq (expected, c, name) = describe ("Compose sequence for " <> unpack name) $ do
+    let c' = T.singleton c
+    let actualSeq = runParser buttonP "" c'
+    let expectedSeq = runParser (KComposeSeq <$> some buttonP) "" expected
+    let actualE2E = parseTokens $ "(deflayer <test> " <> c' <> " )"
+    let expectedE2E = first ParseError expectedSeq <&> \x -> [KDefLayer (DefLayer "<test>" Nothing [x])]
+    it "Is compose sequence" $ actualSeq `shouldSatisfy` parsesAsValidComposeSeq
+    it "Matches expected" $ actualSeq `shouldBe` expectedSeq
+    it "Could parse in E2E" $ actualE2E `shouldBe` expectedE2E
   parsesAsValidComposeSeq (Right (KComposeSeq seq')) = all isSimple seq'
   parsesAsValidComposeSeq _ = False
   isSimple (KEmit _) = True

commit 5d12aead305ff41eab1462d36a702632244dbf50

K.A.Types: Derive Eq for ImplArnd and DefLayerSetting
Fixes: 3ca703c032f80c643fcf4c291a72038251bc68e4
Fixes: https://github.com/kmonad/kmonad/pull/869

src/KMonad/Args/Types.hs
@@ -105,7 +105,7 @@ data ImplArnd
   | IAAround
   | IAAroundOnly
   | IAAroundWhenAlone
-  deriving Show
+  deriving (Show, Eq)
 
 --------------------------------------------------------------------------------
 -- $cfg
@@ -149,7 +149,7 @@ data DefLayerSetting
   = LSrcName Text
   | LImplArnd ImplArnd
   | LButton DefButton
-  deriving Show
+  deriving (Show, Eq)
 makeClassyPrisms ''DefLayerSetting
 
 -- | A layer of buttons