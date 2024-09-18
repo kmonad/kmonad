bb6c80dc2a47247e0e7d92990196b37337f448ca
Joschua Kesper <jokesper@proton.me>
2024-09-18 16:15:44 +0200

    Fixed warnings
    `-Wunused-imports`
    
    - KMonad.Prelude:
      - GHC.Conc (orElse) is exported by RIO via UnliftIO.STM
      - Removed KMonad.Prelude.Definitions as it is empty
    - KMonad.Keyboard.IO.Linux.UinputSink:
      - UnliftIO.Async (async) is exported by KMonad.Prelude via RIO
    - *:
      - KMonad.Keyboard.IO is not needed and should not be needed there
    
    `-Wname-shadowing`
    
    Mark with tick except for KMonad.Args.Cmd (cmd) in KMonad.Args
    since it doesn't add clarity
    
    `-Wx-partial`
    
    - KMonad.Gesture:
      - Use view patterns with `S.lookupMin` instead of `S.null`
        with `head . S.elems` on case failure.
        Cases had to be switched, since old GHC versions don't detect
        exhaustive pattern matching when done by listing all constructors
        in view patterns.
    
    `-Wdodgy-imports`
    - KMonad.Prelude:
      - No longer a warning, since we dropped support for the GHC version in
        question

--- src/KMonad/Prelude/Imports.hs
+++ src/KMonad/Prelude/Imports.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-dodgy-imports #-}
-
 module KMonad.Prelude.Imports
   ( module X )
 where
@@ -7,16 +5,12 @@ where
 import Control.Lens       as X
 import Control.Monad.Cont as X
 import Data.Acquire       as X
-import GHC.Conc           as X (orElse)
 import RIO.Text           as X (unlines, lines, unpack, pack)
 
 import RIO as X hiding
   (-- Not the lens stuff, I want more support for lenses from "Control.Lens"
     view, ASetter, ASetter', Lens, Getting, Lens'
   , SimpleGetter, lens, over, set, sets, to, (^.)
-
-    -- The following line is required for newer stack releases.
-    -- This is also the reason for the OPTIONS_GHC pragma
   , (^..), (^?), preview, (%~), (.~)
 
     -- Some stuff I'd rather default to Text
@@ -24,4 +18,5 @@ import RIO as X hiding
 
     -- Will import these when I need it
   , some, many
+  , seq
   )
