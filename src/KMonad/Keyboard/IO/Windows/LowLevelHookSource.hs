0d930deb2de8ab72d52efd1df5622259379da7e6
Joschua Kesper <jokesper@proton.me>
2024-09-27 14:47:31 +0200

    Fix warning in windows CI
    The return in `keyHandler` would have been, as far as I could tell,
    compiled to a zero.
    I think changing it to non zero to block other event lowlevel handlers
    is the right decision and may fix some bugs, ~~though I am not on Windows
    and am unable to test any of this.~~
    
    After having someone else test it, I can confirm KMonad works just as
    it did before.
    
    Since we ignore the return value from `grab_kb` and don't return
    anything, we just change it to `void`.

--- src/KMonad/Keyboard/IO/Windows/LowLevelHookSource.hs
+++ src/KMonad/Keyboard/IO/Windows/LowLevelHookSource.hs
@@ -27,7 +27,7 @@ import KMonad.Keyboard.IO.Windows.Types
 
 -- | Use the windows c-api to `grab` a keyboard
 foreign import ccall "grab_kb"
-  grab_kb :: IO Word8
+  grab_kb :: IO ()
 
 -- | Release the keyboard hook
 foreign import ccall "release_kb"
@@ -43,7 +43,7 @@ foreign import ccall "wait_key"
 
 -- | Data used to track `connection` to windows process
 data LLHook = LLHook
-  { _thread :: !(Async Word8)        -- ^ The thread-id of the listen-process
+  { _thread :: !(Async ())        -- ^ The thread-id of the listen-process
   , _buffer :: !(Ptr WinKeyEvent) -- ^ Buffer used to communicate with process
   }
 makeLenses ''LLHook
