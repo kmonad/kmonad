6b7ee1434a1fe0432b18f27c0130b2791cc5481b
Joschua Kesper <jokesper@proton.me>
2024-10-10 00:38:26 +0200

    Deduplicate core KMonad data structure
    This commit unifies the following data structures with the help of type families
    - 'DefSetting' -> 'Parsing'
    - 'CfgToken'   -> 'Token'
    - 'JCfg'       -> 'Joining'
    - 'Cmd'        -> 'Cmd'
    - 'AppCfg'     -> 'App'
    - 'AppEnv'     -> 'Env'
    
    Further changes:
    - Config values are merged via a Monoid instance
      => 'joinConfig' can use pattern matching for duplication checking
    - 'output' setting when missing was wrongly reported as 'input'
    - Disallow compose sequences such as 'ä' as a compose key
    - Print the reason why a compose key was invalid
    - You can specify multiple 'defcfg' which get merged
    - You can now disable 'fallthrough' and 'allow-cmd' via the command line.
    - Fixed some 'lexeme' stuff
      - Empty 'defcfg' is a parse error not a join error
      - 'lexeme (lexeme ...)'
    - 'DefSetting' equality instance is now a normal instance, since we no longer
      use it when merging a 'CmdL' and 'PCfg' and only need it for tests.

---
+++ src/KMonad/Model/Cfg.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE NoFieldSelectors #-}
+-- No explicit export list, since we only define a data structure,
+-- which we want to fully export
+module KMonad.Model.Cfg where
+
+import KMonad.Args.Types
+import KMonad.Keyboard
+import KMonad.Model.Button
+import KMonad.Model.Keymap
+import KMonad.Model.TH
+
+data Stage
+  = Parsing  -- ^ While parsing, before joining
+  | Token    -- ^ A joined config
+  | Joining  -- ^ An intermediate config used during joining
+  | Cmd      -- ^ For processing command line arguments
+  | App      -- ^ To start the app
+  | Env      -- ^ While running
+
+type family XSource x where
+  XSource 'Parsing  = [IToken]
+  XSource 'Token    = LogFunc -> IO (Acquire KeySource)
+  XSource 'Joining  = ()
+  XSource 'Cmd      = Maybe IToken
+  XSource 'App      = Acquire KeySource
+  XSource 'Env      = KeySource
+
+type family XSink x where
+  XSink 'Parsing  = [OToken]
+  XSink 'Token    = LogFunc -> IO (Acquire KeySink)
+  XSink 'Joining  = ()
+  XSink 'Cmd      = Maybe OToken
+  XSink 'App      = Acquire KeySink
+  XSink 'Env      = KeySink
+
+type family XLogging x where
+  XLogging 'Cmd  = LogLevel
+  XLogging 'Env  = LogFunc
+  XLogging _     = ()
+
+type family XCmpKey x where
+  XCmpKey 'Parsing  = [DefButton]
+  XCmpKey 'Joining  = Maybe Button
+  XCmpKey 'Cmd      = Maybe DefButton
+  XCmpKey _         = ()
+
+type family XCmpSeqDelay x where
+  XCmpSeqDelay 'Parsing  = [Int]
+  XCmpSeqDelay 'Joining  = Maybe Int
+  XCmpSeqDelay 'Cmd      = Maybe Int
+  XCmpSeqDelay _         = ()
+
+type family XKeymap x where
+  XKeymap 'Parsing  = [KExpr]
+  XKeymap 'Token    = LMap Button
+  XKeymap 'Joining  = [KExpr]
+  XKeymap 'Cmd      = FilePath
+  XKeymap 'App      = LMap Button
+  XKeymap 'Env      = Keymap
+
+type family XFstLayer x where
+  XFstLayer 'Token  = LayerTag
+  XFstLayer 'App    = LayerTag
+  XFstLayer _       = ()
+
+type family XFallThrough x where
+  XFallThrough 'Parsing  = [Bool]
+  XFallThrough 'Joining  = ()
+  XFallThrough 'Cmd      = Maybe Bool
+  XFallThrough _         = Bool
+
+type family XAllowCmd x where
+  XAllowCmd 'Parsing  = [Bool]
+  XAllowCmd 'Joining  = ()
+  XAllowCmd 'Cmd      = Maybe Bool
+  XAllowCmd _         = Bool
+
+type family XStartDelay x where
+  XStartDelay 'Cmd  = Milliseconds
+  XStartDelay 'App  = Milliseconds
+  XStartDelay _     = ()
+
+type family XKeySeqDelay x where
+  XKeySeqDelay 'Parsing  = [Int]
+  XKeySeqDelay 'Token    = Maybe Int
+  XKeySeqDelay 'Joining  = ()
+  XKeySeqDelay 'Cmd      = Maybe Int
+  XKeySeqDelay 'App      = Maybe Milliseconds
+  XKeySeqDelay 'Env      = ()
+
+type family XImplArnd x where
+  XImplArnd 'Parsing  = [ImplArnd]
+  XImplArnd 'Joining  = ImplArnd
+  XImplArnd 'Cmd      = Maybe ImplArnd
+  XImplArnd _         = ()
+
+data Cfg x = Cfg
+  { _source       :: XSource x
+  , _sink         :: XSink x
+  , _logging      :: XLogging x
+  , _cmpKey       :: XCmpKey x
+  , _cmpSeqDelay  :: XCmpSeqDelay x
+  , _keymap       :: XKeymap x
+  , _fstLayer     :: XFstLayer x
+  , _fallThrough  :: XFallThrough x
+  , _allowCmd     :: XAllowCmd x
+  , _startDelay   :: XStartDelay x
+  , _keySeqDelay  :: XKeySeqDelay x
+  , _implArnd     :: XImplArnd x
+  }
+makeClassy ''Cfg
+mkStages ''Cfg
+
+deriving instance Show PCfg
+deriving instance Eq PCfg
+
+instance Semigroup PCfg where
+  PCfg is os cks csds km fts acs ksds ias <> PCfg is' os' cks' csds' km' fts' acs' ksds' ias' =
+    PCfg (is ++ is') (os ++ os') (cks ++ cks') (csds ++ csds') (km ++ km') (fts ++ fts') (acs ++ acs') (ksds ++ ksds') (ias ++ ias')
+
+instance Monoid PCfg where mempty = PCfg [] [] [] [] [] [] [] [] []
