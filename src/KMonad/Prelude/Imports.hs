a90e391d1b6b3319d4147f247bc235fefb3a4830
Joschua Kesper <jokesper@proton.me>
2024-10-10 22:08:11 +0200

    Reduced usage of partial functions

--- src/KMonad/Prelude/Imports.hs
+++ src/KMonad/Prelude/Imports.hs
@@ -9,6 +9,8 @@ import Control.Monad.Cont as X
 import Data.Acquire       as X
 import RIO.Char           as X
 import RIO.Text           as X (unlines, lines, unpack, pack)
+import Data.Foldable1     as X
+import RIO.NonEmpty       as X (tail, nonEmpty, groupAllWith, some1)
 
 import RIO as X hiding
   (-- Not the lens stuff, I want more support for lenses from "Control.Lens"
